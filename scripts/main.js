d2e75a671ad478e65ac7806de1db56bee0046f90
nvrmuxtorov <nvrmuxtorov@gmail.com>
2024-01-29 15:18:19 +0500

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,164 @@
+// let array = [
+//     {
+//         id: 1,
+//         name: 'Timur',
+//         age:6,
+//     },
+//     {
+//         id: 2,
+//         name: 'Imran',
+//       age:8
+//     },
+//     {
+//         id: 3,
+//         name: 'Aminjon',
+//        age:19,
+//     },
+//     {
+//         id: 4,
+//         name: 'Maxmud',
+//        age:4,
+//     },
+//     {
+//         id: 5,
+//         name: 'Muxammad',
+//       age:13
+//     },
+//     {
+//         id: 6,
+//         name: 'Akbar',
+//     age:12
+//     },
+//     {
+//         id: 7,
+//         name: 'Ozod',
+//         age:16,
+//     },
+//     {
+//         id: 8,
+//         name: 'BUnyod',
+//       age:18
+//     },
+//     {
+//         id: 9,
+//         name: 'Amirjon',
+//        age:9,
+//     },
+//     {
+//         id: 10,
+//         name: 'Murod',
+//        age:4,
+//     },
+//     {
+//         id: 11,
+//         name: 'Muxammad',
+//       age:13
+//     },
+//     {
+//         id: 12,
+//         name: 'Olim',
+//     age:5
+//     },
+//     {
+//         id: 13,
+//         name: 'Abror',
+//         age:6,
+//     },
+//     {
+//         id: 14,
+//         name: 'Shohista',
+//       age:8
+//     },
+//     {
+//         id: 15,
+//         name: 'Muhammad',
+//        age:9,
+//     },
+//     {
+//         id: 16,
+//         name: 'Nazakot',
+//        age:20,
+//     },
+//     {
+//         id: 17,
+//         name: 'Akbar',
+//       age:13
+//     },
+//     {
+//         id: 18,
+//         name: 'Baxodir',
+//     age:2
+//     },
+//     {
+//         id: 19,
+//         name: 'Botir',
+//         age:19,
+//     },
+//     {
+//         id: 20,
+//         name: 'Ikrom',
+//       age:8,
+//     },
+//     {
+//         id: 21,
+//         name: 'Shuhrat',
+//        age:21,
+//     },
+//     {
+//         id: 22,
+//         name:'Oydin',
+//        age:5,
+//     },
+//     {
+//         id: 23,
+//         name: 'Mahmud',
+//       age:13,
+//     },
+//     {
+//         id: 24,
+//         name: 'amir',
+//     age:15
+//     },
+//     {
+//         id: 25,
+//         name: 'Abubakr',
+//         age:5,
+//     },
+//     {
+//         id: 26,
+//         name: 'Miron',
+//       age:8,
+//     },
+//     {
+//         id: 27,
+//         name: 'Aziz',
+//        age:10,
+//     },
+//     {
+//         id: 28,
+//         name: 'Akmal',
+//        age:5,
+//     },
+//     {
+//         id: 29,
+//         name: 'Mirodil',
+//       age:3,
+//     },
+//     {
+//         id: 30,
+//         name: 'Akbar',
+//     age:19
+//     },
+// ]
+// let bogcha = array.filter(item=>item.age <=6)
+// let university = array.filter(item=>item.age >=18)
+// let maktab = array.filter( item=>item.age>6 && item.age <18)
+
+// console.log(bogcha,maktab,university);
+
+
+
+// let text=prompt("soz kiriting")
+// arr=text.split(" ")
+// let newarr= arr.filter(item=>item != "False" &&item != "None" &&item != "and"&& item != 0 )
+// console.log(newarr.join(" "));
